c726ff530862f4d1f456d06a1ac30315886ddabe
sofandr <sofandr@mail.ru>
2025-11-04 01:39:07 +0300

    Changes to be committed: 	modified:   template.7z new file:   template/data.yaml 	new file:   template/gen_from_template.py 	new file:   template/install_pip.bat 	new file:   template/output.sh 	new file:   template/template.sh.j2

---
+++ template/gen_from_template.py
@@ -0,0 +1,41 @@
+﻿# 
+# 
+# pip install jinja2
+# pip install pyyaml
+#
+# 
+
+from jinja2 import Environment, FileSystemLoader
+import subprocess
+import yaml  # pip install pyyaml
+from datetime import datetime
+
+# Текущая дата и время
+current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Формат: 2025-11-04 15:30:45
+
+
+# 1. Загружаем данные из YAML-файла
+with open("data.yaml", "r", encoding="utf-8") as file:
+    data = yaml.safe_load(file)
+
+# 2. Добавляем текущую дату и время в данные
+data["current_datetime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+data["date_short"] = datetime.now().strftime("%Y-%m-%d")  # Краткая дата
+data["time"] = datetime.now().strftime("%H:%M:%S")        # Время
+
+
+# 2. Настройка окружения Jinja2
+env = Environment(loader=FileSystemLoader('.'))
+env.filters['shell'] = lambda cmd: subprocess.check_output(cmd, shell=True, text=True).strip()
+
+# 3. Загружаем и рендерим шаблон
+template = env.get_template("template.sh.j2")
+rendered_script = template.render(**data)
+
+# 4. Выводим и сохраняем результат
+print("Сгенерированный скрипт:")
+print(rendered_script)
+
+with open("output.sh", "w", encoding="utf-8") as file:
+    file.write(rendered_script)
+
---
+++ template/install_pip.bat
@@ -0,0 +1,2 @@
+pip install jinja2
+pip install pyyaml
